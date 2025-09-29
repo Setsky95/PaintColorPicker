188bb087996bede79898719c4bcc99a4f0c9d8d1
Sebastian Pavlotsky <sebapavlotsky@gmail.com>
2025-09-29 11:43:17 -0300

    atributos

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,13 +1,13 @@
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import rehypeRaw from 'rehype-raw';
-import './data.css';
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import "./data.css";
 
-export default function Data
-({ name, code, hex, description, galeria }) {
+export default function Data({ name, code, hex, description, galeria }) {
   if (!name && !code) return null;
 
-  const color = hex ?? (code ? (code.startsWith("#") ? code : `#${code}`) : "#000");
+  const color =
+    hex ?? (code ? (code.startsWith("#") ? code : `#${code}`) : "#000");
 
   return (
     <div className="cw-legend">
@@ -17,13 +17,32 @@ export default function Data
 
       {code && <div className="cw-legend-code">{code}</div>}
 
-
-
       {description && (
         <div className="cw-legend-description">
           <ReactMarkdown>{description}</ReactMarkdown>
         </div>
       )}
+      <div className="data-atributos">
+        <span className="data-atributo">Lavable</span>
+        <span className="data-atributo">Alta adherencia</span>
+        <span className="data-atributo">Impermeable</span>
+      </div>
+      {/*  <div className="data-buttons">
+        <select className="data-atributo">
+          <option value="1lt">1 lt</option>
+          <option value="5lt">5 lt</option>
+          <option value="20lt">20 lt</option>
+        </select>
+        <div className="agregarAlCarrito">
+          <button
+            className="data-atributo"
+            style={{ backgroundColor: color }}
+            onClick={() => alert("agregado ")} 
+          >
+            Agregar al carrito 
+          </button>
+        </div>
+      </div> */}
     </div>
   );
 }
